0e6d2c0c7cc982b121f0a6d096e741b0207e8a0d
Vadimych1 <vadimlebdenko0@gmail.com>
2024-02-12 18:05:21 +0300

    upd

--- UchetSklada.py
+++ UchetSklada.py
@@ -53,13 +53,58 @@ import sqlite3
 # con.commit()
 # con.close()
 # print(cur.execute("SELECT * FROM movie").fetchall())
+
+import json
+
+def create_product_table(cur: sqlite3.Cursor, name: str, displayName: str, desc: str) -> None:
+    global tables
+
+    try:
+        cur.execute("CREATE TABLE %s (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, name TEXT NOT NULL, count INTEGER NOT NULL, cost INTEGER NOT NULL, status TEXT, date DATETIME)".format(name))
+        with open("tables.json") as f:
+            t = json.load(f)
+
+        with open("tables.json") as f:
+            t.append({"name": name, "displayName": displayName, "description": desc})
+            json.dump(t)
+
+        tables = t
+    except:
+        pass
+    
+def init_tables(cur: sqlite3.Cursor, tables: list) -> None:
+    for table in tables:
+        cur.execute("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, name TEXT NOT NULL, count INTEGER NOT NULL, cost INTEGER NOT NULL, status TEXT, date DATETIME)".format(table["name"]))
+
+def fetch_from_product_tables(cur: sqlite3.Cursor, tableName: str, count: int | None = None) -> list:
+    if count == None:
+        cur.execute("SELECT * FROM %s".format(tableName))
+    else:
+        cur.execute("SELECT * FROM %s LIMIT %i".format(tableName, count))
+
+    return cur.fetchall()
+
+# ! Connect to database
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
-cur.execute("""
-    INSERT INTO movie VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
-""")
+
+# ! Read settings from JSON:
+with open("settings.json", "r") as f:
+    settings = json.load(f)
+
+# ! Read database tables from JSON:
+with open("tables.json", "r") as f:
+    tables = json.load(f)
+
+# ! Initialize tables
+init_tables(cur, tables)
 con.commit()
-print(cur.execute("SELECT * FROM movie").fetchall())
+
+# cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+# cur.execute("""
+#     INSERT INTO movie VALUES
+#         ('Monty Python and the Holy Grail', 1975, 8.2),
+#         ('And Now for Something Completely Different', 1971, 7.5)
+# """)
+# con.commit()
+# print(cur.execute("SELECT * FROM movie").fetchall())
